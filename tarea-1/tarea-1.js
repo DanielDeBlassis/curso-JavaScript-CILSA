3098d544ab501d0cb2e3e1a33fa1e6c851a4c2cf
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-10-12 09:13:59 -0300

    Crea función para imprimir los datos
    Al hacer click en el botón "Enviar" del formulario, la función toma los valores contenidos en los inputs del formulario, los guarda en variables y luego los retorna incrustándolos con formato HTML en el contenedor del resultado previamente obtenido.

--- tarea-1/tarea-1.js
+++ tarea-1/tarea-1.js
@@ -1,2 +1,15 @@
 const $containerResultado = document.querySelector(".container-resultado");
 
+/**
+ * Imprime los datos introducidos por el usuario
+ */
+function imprimirDatos() {
+    const $nombre = document.querySelector("input[name='nombre']").value;
+    // $nombre.addEventListener("keyup", ()=> console.log($nombre.value));
+    const $apellido = document.querySelector("input[name='apellido']").value;
+    const $edad = Number.parseInt(document.querySelector("input[name='edad']").value);
+
+    return $containerResultado.innerHTML = `<p><em>Nombre</em>: <b>${$nombre}</b></p>
+                                            <p><em>Apellido</em>: <b>${$apellido}</b></p>
+                                            <p><em>Edad</em>: <b>${$edad}</b></p>`;
+}
