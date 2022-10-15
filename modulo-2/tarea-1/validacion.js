3984015bdbcfc5d78b3d02ba43aa44581edaaeee
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-10-15 19:40:43 -0300

    Crea función para validar formulario
    La misma obtiene el botón, y lo deshabilita como primer medida.
    Luego se obtienen todos los elementos input del documento, se iteran y a cada uno se le agrega una escucha al evento "change".
    Cada vez que el estado de los inputs cambien, la función comprobará que los mismos no estén vacíos.
    Si ningún input está vacío, se habilitará el botón para enviar la información e imprimir los datos.

--- modulo-2/tarea-1/validacion.js
+++ modulo-2/tarea-1/validacion.js
@@ -1,2 +1,25 @@
+/**
+ * Valida el formulario comprobando que los campos no estén vacíos
+ */
+const $boton = document.querySelector("#boton-enviar")
+$boton.setAttribute("disabled", "disabled");
+
+const $datos = document.querySelectorAll("input");
+$datos.forEach(dato => {
+    dato.addEventListener("change", () => {
+
+        const $nombre = document.querySelector("[name='nombre']").value;
+        const $apellido = document.querySelector("[name='apellido']").value;
+        const $edad = document.querySelector("[name='edad']").value;
+        const $ciudad = document.querySelector("[name='ciudad']").value;
+
+        if ($nombre != "" && $apellido != "" && $edad != "" && $ciudad != "") {
+            $boton.removeAttribute("disabled");
+        } else {
+            $boton.setAttribute("disabled", "disabled");
+        }
+
+    })
+});
 
 
