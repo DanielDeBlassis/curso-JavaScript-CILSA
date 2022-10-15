71bc46b53dd50d9f54f2767d5f6c03e71f12fce4
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-10-15 19:35:44 -0300

    Crea función para mostrar e imprimir datos
    La misma se activará al hacer click en el botón enviar del formulario.
    Lo primero que hace es prevenir el comportamiento por defecto del botón tipo submit en el formulario, por lo cual no se recarga la página.
    Luego, se obtienen todos los inputs's presentes en el documento. Se crea una variable de tipo arreglo, vacío.
    Se iteran los input's mediante forEach, y se van agregando al arreglo previamente creado.
    Lo siguiente es obtener el contenedor de la información, donde se imprimirá la información en el documento, borrándole el contenido o estableciendo como una cadena de texto vacía, previniendo que se impriman los datos repetidamente en el documento.
    Finalmente se crea un nuevo elemento de tipo párrafo, que se guarda en una variable, a la cual se le asigna la clase parrafo y luego se le asigna como valor una cadena de texto conteniendo la información recogida en el formulario y previamente guardada en el arreglo.
    Finalmente se le agrega el nuevo elemento al contenedor de la información.

--- modulo-2/tarea-1/tarea-1.js
+++ modulo-2/tarea-1/tarea-1.js
@@ -1,3 +1,22 @@
+/**
+ * Muestra los datos ingresados en el formulario
+ * @param {*Event} event tipo de evento, en este caso "click"
+ */
+function mostrarDatos(event) {
+    event.preventDefault();
+
+    const $datos = document.querySelectorAll("input");
+    const arregloDatos = [];
+    $datos.forEach(dato => { arregloDatos.push(dato.value); });
+
+    const $contenedorInformacion = document.querySelector("#info-container");
+    $contenedorInformacion.innerHTML = ``;
+
+    const parrafo = document.createElement("p");
+    parrafo.classList.add("parrafo");
+    parrafo.textContent = `Hola ${arregloDatos[0]} ${arregloDatos[1]}! Así que tenés ${arregloDatos[2]} años y sos de ${arregloDatos[3]}...Bienvenido!`;
+    $contenedorInformacion.appendChild(parrafo);
+}
 
 
 
