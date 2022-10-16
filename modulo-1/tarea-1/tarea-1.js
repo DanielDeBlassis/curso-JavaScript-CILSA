5fbdcbedd8ee8926afc3cce24f801aa56afa2a7e
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-10-16 01:10:28 -0300

    Corrige formato de resultado
    Se agrega un subtítulo y se cumple con el formato solicitado en el ejercicio.

--- modulo-1/tarea-1/tarea-1.js
+++ modulo-1/tarea-1/tarea-1.js
@@ -9,7 +9,8 @@ function imprimirDatos() {
     const $apellido = document.querySelector("input[name='apellido']").value;
     const $edad = Number.parseInt(document.querySelector("input[name='edad']").value);
 
-    return $containerResultado.innerHTML = `<p><em>Nombre</em>: <b>${$nombre}</b></p>
-                                            <p><em>Apellido</em>: <b>${$apellido}</b></p>
-                                            <p><em>Edad</em>: <b>${$edad}</b></p>`;
+    return $containerResultado.innerHTML = `<h3>Usted se ha registrado con los siguientes datos:</h3>
+                                            <p><em>-Nombre</em>: <b>${$nombre}</b></p>
+                                            <p><em>-Apellido</em>: <b>${$apellido}</b></p>
+                                            <p><em>-Edad</em>: <b>${$edad}</b></p>`;
 }
