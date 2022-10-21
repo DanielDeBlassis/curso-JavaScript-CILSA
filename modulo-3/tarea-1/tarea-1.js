5575f0baf09259cd410765e2a551e34a91491b49
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-10-21 17:10:34 -0300

    Crea función para imprimir la opción elegida
    La misma tiene una variable mensaje que variará dependiendo de la elección que se le pase como argumento. Luego se obtiene el área del documento donde se imprimirá el mensaje, para luego insertarle un párrafo con el resultado de la elección y su mensaje.

--- modulo-3/tarea-1/tarea-1.js
+++ modulo-3/tarea-1/tarea-1.js
@@ -11,3 +11,13 @@ function actualizarEleccion() {
     imprimirMensaje($eleccion);
 }
 
+/**
+ * Obtiene el contenedor del resultado previamente creado e imprime el resultado de la elección
+ * @param {string} eleccion el valor del input de tipo check seleccionado
+ */
+function imprimirMensaje(eleccion) {
+    let mensaje = eleccion === "argentina" ? "El mundial lo ganará" : "Según usted, el mundial lo ganará";
+    const mensajeCheck = document.querySelector(".contenedor-mensaje");
+    mensajeCheck.innerHTML = `<p class="ganador-elegido ${eleccion}">${mensaje} <span class="eleccion ${eleccion}">${eleccion}</span> <img src="img/${eleccion}.png" class="flag-2 ${eleccion}" alt="Bandera de ${eleccion}"/></p>`;
+}
+
