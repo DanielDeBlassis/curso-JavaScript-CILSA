54ace4be5d0e266f975804982c9e848eb7cd6f9f
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-07 14:05:49 -0300

    Crea función para obtener nombre y mostrar mensaje
    La misma se ejecuta apenas se carga la página, pidiendo nombre completo mediante un prompt personalizado con sweetalert.
    Luego, agrega el nombre al mensaje para personalizarlo si es que se ingresó.
    También se agrega el número de enlaces al contenedor, al cual al sacarle la clase oculto deja expuesta la información.

--- modulo-5/tarea-1/tarea-1.js
+++ modulo-5/tarea-1/tarea-1.js
@@ -3,3 +3,33 @@ let apellidoIngresado = "";
 
 let mensaje = document.createElement("p");
 
+window.onload = function () {
+
+    const $contenedorMensaje = document.querySelector(".container__mensaje");
+
+    const $info = document.getElementById("infopag");
+    const $enlaces = document.getElementsByTagName("a");
+
+    //Mensaje de bienvenida más nombre ingresado (Pista: usar innerHTML)
+    swal("Ingresa tu nombre:", {
+        content: "input",
+    })
+        .then((nombre) => {
+            nombreIngresado = `${nombre}`;
+            mensaje.textContent = nombreIngresado != "null" && nombreIngresado.length != 0 ? `Gracias por visitar nuestra página ${nombreIngresado}!` : `Gracias por visitar nuestra página!`;
+            $contenedorMensaje.insertBefore(mensaje, $contenedorMensaje.children[0]);
+            swal("Ingresa tu apellido:", {
+                content: "input",
+            })
+                .then((apellido) => {
+                    apellidoIngresado = `${apellido}`;
+                    $contenedorMensaje.classList.remove("oculto");
+                });
+        });
+
+    //Número de enlaces que tiene la página (Pista: usar innerHTML)
+    let pEnlaces = document.createElement("p");
+    pEnlaces.textContent = `Número de enlaces = ${$enlaces.length}`;
+    $contenedorMensaje.appendChild(pEnlaces);
+
+}
